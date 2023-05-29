54f7b4f452292cb601f265724567d105e2c9a2c6
awadesu <133776560+awadesu@users.noreply.github.com>
2023-05-29 16:19:58 +0200

    lay calculator structure

--- script.js
+++ script.js
@@ -13,3 +13,38 @@ function multiply(a,b){
 function divide(a,b){
     return a/b
 }
+
+let n;
+let m;
+let o;
+
+function operate(a,b,op){
+    if(op=="+") return add(a,b)
+    if(op=="-") return substract(a,b)
+    if(op=="*") return multiply(a,b)
+    if(op=="/") return divide(a,b)
+}
+
+const body = document.querySelector("body");
+
+const calculator = document.createElement("div");
+calculator.id = "calculator";
+
+const display = document.createElement("div");
+display.id = "display";
+display.innerText="1234567890"
+
+const keys = document.createElement("div");
+keys.id = "keys";
+
+const digits = document.createElement("div");
+digits.id = "digits";
+
+const operators = document.createElement('div');
+operators.id = "operators";
+
+keys.appendChild(digits);
+keys.appendChild(operators);
+calculator.appendChild(display);
+calculator.appendChild(keys);
+body.appendChild(calculator);
