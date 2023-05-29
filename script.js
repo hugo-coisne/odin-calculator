7b33a3d5eb207f4f2085c5a6ef48b0bec408d8bb
awadesu <133776560+awadesu@users.noreply.github.com>
2023-05-29 17:02:03 +0200

    add digits

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
-function add(a,b){
-    return a+b
+function add(a, b) {
+    return a + b;
 }
 
-function substract(a,b){
-    return a-b
+function substract(a, b) {
+    return a - b;
 }
 
-function multiply(a,b){
-    return a*b
+function multiply(a, b) {
+    return a * b;
 }
 
-function divide(a,b){
-    return a/b
+function divide(a, b) {
+    return a / b;
 }
 
 let n;
 let m;
 let o;
 
-function operate(a,b,op){
-    if(op=="+") return add(a,b)
-    if(op=="-") return substract(a,b)
-    if(op=="*") return multiply(a,b)
-    if(op=="/") return divide(a,b)
+function operate(a, b, op) {
+    if (op == "+") return add(a, b);
+    if (op == "-") return substract(a, b);
+    if (op == "*") return multiply(a, b);
+    if (op == "/") return divide(a, b);
 }
 
 const body = document.querySelector("body");
@@ -32,7 +32,7 @@ calculator.id = "calculator";
 
 const display = document.createElement("div");
 display.id = "display";
-display.innerText="1234567890"
+display.innerText = "1234567890";
 
 const keys = document.createElement("div");
 keys.id = "keys";
@@ -42,6 +42,29 @@ digits.id = "digits";
 
 const operators = document.createElement('div');
 operators.id = "operators";
+
+for (let j = 9; j >= 0; j -= 3) {
+    const row = document.createElement("div");
+    row.classList = "row";
+    for (let i = 0; i <= 2; i++) {
+        if (j - i >= 0) {
+            const digit = document.createElement('div');
+            digit.innerText = j - i;
+            digit.classList = `key`;
+            digit.id = j - i;
+            row.appendChild(digit);
+        }
+    }
+    if(row.childElementCount==1){
+        const digit = document.createElement('div');
+        digit.classList = `key`;
+        digit.id = "clear";
+        digit.innerText = "Clear";
+        row.appendChild(digit);
+    }
+    digits.appendChild(row);
+}
+
 
 keys.appendChild(digits);
 keys.appendChild(operators);
