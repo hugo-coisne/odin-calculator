619eceff1ff65ab02ce72ca8932d5631fdd76e53
awadesu <133776560+awadesu@users.noreply.github.com>
2023-05-31 08:57:08 +0200

    remove all display change events for fresh start

--- script.js
+++ script.js
@@ -72,26 +72,11 @@ function calculator() {
             digit.classList = `key`;
             digit.id = "dot";
             digit.innerText = ".";
-            digit.addEventListener('click', (e) => {
-                if (o == "") {
-                    o = "0.";
-                } else if(o.includes(".")){
-                    o="error";
-                }else {
-                    o += ".";
-                }
-                display.innerText = o;
-            });
             row.appendChild(digit);
             digit = document.createElement('div');
             digit.classList = `key`;
             digit.id = "clear";
             digit.innerText = "Clear";
-            digit.addEventListener('click', (e) => {
-                o = "";
-                first = true;
-                display.innerText = o; //clear event
-            });
             row.appendChild(digit);
         }
         digits.appendChild(row);
@@ -101,27 +86,6 @@ function calculator() {
         const op = document.createElement("div");
         op.classList = "key";
         op.innerText = "+-*/="[el];
-        op.addEventListener('click', (e) => {
-            if (first && e.target.innerText == "=") {
-                o = "error";
-                display.innerText = o;
-            } else if (first) {
-                ope = e.target.innerText;
-                first = false;
-                n = Number(o);
-                console.log(n);
-                o="";
-            } else if (!first) {
-                first = true;
-                m = Number(o);
-                console.log(n, m, ope);
-                o = Math.round(operate(n, m, ope) * 1000) / 1000;
-                console.log(o);
-                display.innerText = o;
-                ope = e.target.innerText;
-            }
-
-        });
         operators.appendChild(op);
     }
 
@@ -139,18 +103,3 @@ allKeys.forEach(k => {
     k.addEventListener('click', e => addPressed(e.target));
     k.addEventListener('transitionend', e => removePressed(e.target));
 }); //key-click animation
-
-const numbers = document.querySelectorAll(".key[key]");
-console.log(Array.from(numbers).map(el => el.innerText));
-numbers.forEach(key => key.addEventListener('click', (e) => {
-    if (first && !"+-*/=".includes(o[o.length - 1])) {
-        o += e.target.innerText;
-        display.innerText = o;
-    } else {
-        o = "";
-        o += e.target.innerText;
-        display.innerText = o;
-    }
-
-})); //key-click changes display
-
